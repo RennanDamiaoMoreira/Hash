64aa2e48f2d0fa204270f03ee1668530221a4f85
Rennan Damião Moreira <50747648+RennanDamiaoMoreira@users.noreply.github.com>
2021-08-10 20:18:29 -0300

    Add files via upload

---
+++ src/model/Chaining.java
@@ -0,0 +1,94 @@
+package model;
+
+public class Chaining {
+    private int tamanho;
+    private Elo list[];
+
+    public Chaining(int tamanho) {
+        this.tamanho = tamanho;
+        this.list = new Elo[tamanho];
+    }
+    public void addValue(String value){
+        int charCode=0;
+        int key;
+        String returnText = "String == "+value+"\n";
+        for (int i=0; i<value.length();i++){
+            charCode+=(int)value.charAt(i);
+            returnText += value.charAt(i)+"=="+(int)value.charAt(i)+"  SubTotal == "+charCode+"\n";
+        }
+        returnText+="\n\nTotal = "+charCode;
+        key = charCode%tamanho;
+        returnText+="  chave "+key+"\n\n";
+        if (list[key]==null){
+            list[key]=new Elo();
+        }
+        System.out.println(returnText);
+        list[key].add(value);
+
+    }
+
+    public void removeValue(String value){
+        int charCode=0;
+        int key;
+        String returnText = "String == "+value+"\n";
+        for (int i=0; i<value.length();i++){
+            charCode+=(int)value.charAt(i);
+            returnText += value.charAt(i)+"=="+(int)value.charAt(i)+"  SubTotal == "+charCode+"\n";
+        }
+        returnText+="\n\nTotal = "+charCode;
+        key = charCode%tamanho;
+        returnText+="  chave "+key+"\n\n";
+
+        System.out.println(returnText);
+        if (list[key]==null){
+            System.out.println("Elemento nao encontrado");
+        }else{
+            if(list[key].getValue()==value){
+                list[key]=list[key].getProximo();
+                System.out.println("Elemento removido");
+            }else{
+                list[key].delete(value);
+            }
+        }
+
+
+
+    }
+    public void searchValue (String value){
+        int charCode=0;
+        int key;
+        String returnText = "String == "+value+"\n";
+        for (int i=0; i<value.length();i++){
+            charCode+=(int)value.charAt(i);
+            returnText += value.charAt(i)+"=="+(int)value.charAt(i)+"  SubTotal == "+charCode+"\n";
+        }
+        returnText+="\n\nTotal = "+charCode;
+        key = charCode%tamanho;
+        returnText+="  chave "+key+"\n\n";
+        System.out.println(returnText);
+        if (list[key]==null){
+            System.out.println("Não encontrado");;
+        }else{
+            if (list[key].search(value)){
+                System.out.println("Valor encontrado");
+            }else{
+                System.out.println("Não encontrado");
+            }
+        }
+
+
+    }
+
+    public void gerarRepresentacao(){
+        String returnText = "";
+        for (int i = 0 ; i<tamanho;i++){
+            returnText = returnText +i+"|";
+            if(list[i]!=null){
+                returnText=returnText+list[i].getProgress();
+            }
+            returnText=returnText +"\n";
+        }
+        System.out.println(returnText);
+    }
+
+}
---
+++ src/model/Elo.java
@@ -0,0 +1,88 @@
+package model;
+
+public class Elo {
+
+    private  String value;
+    private Elo proximo;
+    private Elo anterior;
+
+    public Elo getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(Elo anterior) {
+        this.anterior = anterior;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Elo getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(Elo proximo) {
+        this.proximo = proximo;
+    }
+
+    public String getProgress() {
+        if (this.getProximo()==null){
+            return getValue();
+        }
+        return this.getValue()+"=>"+this.getProximo().getProgress();
+    }
+
+    public void add(String value) {
+        if (this.getValue()==null||this.getValue().isBlank()) {
+            this.setValue(value);
+            System.out.println("posicao primaria livre");
+           return;
+        }
+       if (this.getProximo()!=null){
+           System.out.println("Posicao ocupada, proximo elo ");
+           this.getProximo().add(value);
+       }else{
+           Elo temp = new Elo();
+           this.setProximo(temp);
+           temp.setValue(value);
+           temp.setAnterior(this);
+           System.out.println("Posicao ocupada, proximo elo ");
+       }
+    }
+
+
+
+    public void delete(String value) {
+
+        if(this.getValue()==value){
+            this.getAnterior().setProximo(this.getProximo());
+            System.out.println("Elemento removido");
+        }
+        else{
+            if (this.getProximo()==null){
+                System.out.println("Elemento não encontrado");
+            }else{
+                this.getProximo().delete(value);
+            }
+        }
+    }
+
+
+
+    public boolean search(String value) {
+        if (this.getValue()==value){
+            return true;
+        }else{
+            if(this.getProximo()!=null){
+                return this.getProximo().search(value);
+            }else{
+                return false;
+            }
+        }
+    }
+}
